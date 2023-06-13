383ac0f74b0ef247729225d1a97878a40831b62b
Behnam <behnamborhani95@gmail.com>
2023-06-13 23:35:13 +0330

    build: base of card section

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./card.scss";
 
 class Card extends Component {
-  render() {
-    return (
-      <div>Card</div>
-    )
-  }
+   render() {
+      const { movie } = this.props;
+      return (
+         <>
+            <div className="card">
+               <div className="poster">
+                  <img src={movie.Poster} alt={movie.Title} />
+               </div>
+               <h2 className="title">{movie.Title}</h2>
+               <p className="information">
+                  <span>{movie.Director}</span>
+                  <span>{movie.Year}</span>
+               </p>
+               <p className="genre">{movie.Genre}</p>
+            </div>
+         </>
+      );
+   }
 }
 export default Card;
--- src/components/movieSection/MovieSection.js
+++ src/components/movieSection/MovieSection.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
 import "./movieSection.scss";
+import axios from "axios";
+import Card from "../card/Card";
 import SearchBox from "../searchBox/SearchBox";
 
 class MovieSection extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         movies: [],
+      };
+   }
+
+   componentDidMount = () => {
+      axios
+         .get("./movies.json")
+         .then((res) => this.setState({ movies: res.data }));
+   };
+
    render() {
       return (
          <>
             <section className="container">
-               <div className="card-container"></div>
+               <div className="card-container">
+                  {this.state.movies.map((movie) => {
+                     return <Card movie={movie} />;
+                  })}
+               </div>
                <SearchBox />
             </section>
          </>
