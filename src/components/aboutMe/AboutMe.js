eb6841ee96e57cf5cd0a367ee4100c9e38f694df
Behnam <behnamborhani95@gmail.com>
2023-06-13 21:55:25 +0330

    base code created

---
+++ src/components/aboutMe/AboutMe.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./aboutMe.scss";
+
+class AboutMe extends Component {
+  render() {
+    return (
+      <div>AboutMe</div>
+    )
+  }
+}
+
+export default AboutMe;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "./navbar.scss";
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>Navbar</div>
+    )
+  }
+}
+
+export default Navbar;
